9f2b705b9ea96050a774dc577529e0f4024760c3
menashekoren <menashekoren@gmail.com>
2021-03-31 10:58:33 +0300

    Add views for admin/ and recipes/

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['name']}
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
